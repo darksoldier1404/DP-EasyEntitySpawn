9dbf3635435e6f9159adbaf888f654c1062c56fd
darksoldier1404 <darksoldier1404@gmail.com>
2022-07-02 21:27:23 +0900

    1.19 update

---
+++ v1_12_2/src/main/java/com/darksoldier1404/dees/events/DEESEvent.java
@@ -0,0 +1,39 @@
+package com.darksoldier1404.dees.events;
+
+import com.darksoldier1404.dees.EasyEntitySpawn;
+import com.darksoldier1404.dees.functions.DEESFunction;
+import com.darksoldier1404.dppc.api.inventory.DInventory;
+import com.darksoldier1404.dppc.utils.NBT;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class DEESEvent implements Listener {
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+        if(e.getRawSlot() < 54) {
+            if(DEESFunction.currentInv.containsKey(e.getWhoClicked().getUniqueId())) {
+                DInventory di = DEESFunction.currentInv.get(e.getWhoClicked().getUniqueId());
+                if(di.isValidHandler(EasyEntitySpawn.getInstance())) {
+                    e.setCancelled(true);
+                    if(e.getCurrentItem() != null) {
+                        if (NBT.hasTagKey(e.getCurrentItem(), "prev")) {
+                            di.prevPage();
+                            return;
+                        }
+                        if (NBT.hasTagKey(e.getCurrentItem(), "next")) {
+                            di.nextPage();
+                            return;
+                        }
+                        if (NBT.hasTagKey(e.getCurrentItem(), "spawn")) {
+                            String name = NBT.getStringTag(e.getCurrentItem(), "spawn");
+                            e.getWhoClicked().getWorld().spawnEntity(e.getWhoClicked().getLocation(), EntityType.valueOf(name));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
